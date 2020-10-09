11e982acbef63c0ecd070a880c4f759e232ba4ac
zlyuancn <zlyuancn@163.com>
2020-10-09 10:16:01 +0800

    defaultManager改为singleManager

--- single.go
+++ single.go
@@ -14,41 +14,41 @@ import (
 	"github.com/spf13/viper"
 )
 
-var defaultManager = NewManager()
+var singleManager = NewManager()
 
 // 添加配置, 同一个conn类型中重复的conn名会被替换掉
 func AddConfig(conntype ConnType, config interface{}, conn_name ...string) {
-	defaultManager.AddConfig(conntype, config, conn_name...)
+	singleManager.AddConfig(conntype, config, conn_name...)
 }
 
 // 移除, 移除之前会关闭连接
 func Remove(conntype ConnType, conn_name ...string) {
-	defaultManager.Remove(conntype, conn_name...)
+	singleManager.Remove(conntype, conn_name...)
 }
 
 // 连接所有
 func ConnectAll() error {
-	return defaultManager.ConnectAll()
+	return singleManager.ConnectAll()
 }
 
 // 关闭所有连接
 func CloseAll() {
-	defaultManager.CloseAll()
+	singleManager.CloseAll()
 }
 
 // 获取Conn
 func GetConn(conntype ConnType, conn_name ...string) (conn *Conn, ok bool) {
-	return defaultManager.GetConn(conntype, conn_name...)
+	return singleManager.GetConn(conntype, conn_name...)
 }
 
 // 添加配置文件, 支持 json, ini, toml, yaml等
 func AddFile(file string, filetype ...string) error {
-	return defaultManager.AddFile(file, filetype...)
+	return singleManager.AddFile(file, filetype...)
 }
 
 // 添加viper树
 func AddViperTree(tree *viper.Viper) error {
-	return defaultManager.AddViperTree(tree)
+	return singleManager.AddViperTree(tree)
 }
 
 func panicOnErr(err error) {
