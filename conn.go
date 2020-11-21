49602235cd89a1ed74385b2fde5dd120fea3f233
zlyuancn <zlyuancn@163.com>
2020-11-21 20:59:36 +0800

    优化conn并发逻辑

--- conn.go
+++ conn.go
@@ -14,20 +14,23 @@ import (
 
 // conn
 type Conn struct {
-	connType    ConnType
-	instance    interface{} // 实例
-	config      interface{} // 配置
-	isConnected bool        // 状态
+	connType ConnType
+	config   interface{} // 配置
 
 	mx sync.RWMutex
+	wg *connWaitGroup
+}
+
+type connWaitGroup struct {
+	instance interface{}
+	e        error
+	wg       sync.WaitGroup
 }
 
 func newConn(conntype ConnType, configPtr interface{}) *Conn {
 	return &Conn{
-		connType:    conntype,
-		instance:    nil,
-		config:      configPtr,
-		isConnected: false,
+		connType: conntype,
+		config:   configPtr,
 	}
 }
 
@@ -39,12 +42,15 @@ func (m *Conn) Type() ConnType {
 // 实例
 func (m *Conn) Instance() interface{} {
 	m.mx.RLock()
-	defer m.mx.RUnlock()
+	wg := m.wg
+	m.mx.RUnlock()
 
-	if m.isConnected {
-		return m.instance
+	if wg == nil {
+		return nil
 	}
-	return nil
+
+	wg.wg.Wait()
+	return wg.instance
 }
 
 // 配置
@@ -55,48 +61,82 @@ func (m *Conn) Config() interface{} {
 // 是否已连接
 func (m *Conn) IsConnect() bool {
 	m.mx.RLock()
-	defer m.mx.RUnlock()
-	return m.isConnected
+	wg := m.wg
+	m.mx.RUnlock()
+
+	if wg == nil {
+		return false
+	}
+
+	wg.wg.Wait()
+	return wg.e == nil
 }
 
 // 连接
 func (m *Conn) Connect() error {
-	if m.IsConnect() {
-		return nil
+	m.mx.RLock()
+	wg := m.wg
+	m.mx.RUnlock()
+
+	if wg != nil {
+		wg.wg.Wait()
+		return wg.e
 	}
 
 	m.mx.Lock()
-	defer m.mx.Unlock()
-
-	if m.isConnected {
-		return nil
+	wg = m.wg
+	if wg != nil {
+		m.mx.Unlock()
+		wg.wg.Wait()
+		return wg.e
 	}
+
+	wg = new(connWaitGroup)
+	wg.wg.Add(1)
+	defer wg.wg.Done()
+	m.wg = wg
+	m.mx.Unlock()
 
 	instance, err := mustGetConnector(m.connType).Connect(m.config)
 	if err != nil {
+		wg.e = err
+		m.mx.Lock()
+		m.wg = nil
+		m.mx.Unlock()
 		return err
 	}
 
-	m.instance = instance
-	m.isConnected = true
+	wg.instance = instance
 	return nil
 }
 
 // 关闭连接
 func (m *Conn) Close() error {
-	if !m.IsConnect() {
+	m.mx.RLock()
+	wg := m.wg
+	m.mx.RUnlock()
+
+	if wg == nil {
+		return nil
+	}
+	wg.wg.Wait()
+	if wg.e != nil { // 没有连上
 		return nil
 	}
 
 	m.mx.Lock()
-	defer m.mx.Unlock()
+	wg = m.wg
+	if wg == nil {
+		m.mx.Unlock()
+		return nil
+	}
+	m.wg = nil
+	m.mx.Unlock()
 
-	if !m.isConnected {
+	wg.wg.Wait()
+	if wg.e != nil { // 没有连上
 		return nil
 	}
 
-	m.isConnected = false
-	m.instance = nil
-
-	return mustGetConnector(m.connType).Close(m.instance)
+	return mustGetConnector(m.connType).Close(wg.instance)
 }
